0c69727466a07c9d77a51dc84a8f6fb00f526e78
zuston <zuston@zuston.lan>
2015-04-27 19:57:51 +0800

    remove some files

--- content-submit.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once 'redis-demo/RedisController.php';
-	$value=$_REQUEST['value'];
-	// $value='fuck'.time();
-	$redis=new RedisController();
-	$redis->setMessageSS('user1',$value);
-	$arr=array('success'=>'1','name'=>'zuston','text'=>$value);
-    echo json_encode($arr);
--- loader.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	function __autoload($classname)  
-	{  
- 		$class_file =$classname.".php";  
- 		if (file_exists($class_file)){  
- 		require_once 'redis-demo/RedisController.php';  
- 		}  
-	}
